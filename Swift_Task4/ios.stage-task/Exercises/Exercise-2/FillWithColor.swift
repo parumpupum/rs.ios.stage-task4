6dcf437c2c1e5bfcb6b6a52de78415e14ef44f34
Kirill Hobyan <82779687+parumpupum@users.noreply.github.com>
2021-06-07 16:17:24 +0300

    Update FillWithColor.swift

--- Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
@@ -3,6 +3,48 @@ import Foundation
 final class FillWithColor {
     
     func fillWithColor(_ image: [[Int]], _ row: Int, _ column: Int, _ newColor: Int) -> [[Int]] {
-        [[]]
+        
+        let rows = image.count
+        let columns = image[0].count
+        
+        var result = image
+        
+        var visited: [[Bool]] = [[]]
+        visited = Array(repeating: Array(repeating: false, count: columns), count: rows)
+        
+        var nextX : [Int] = [], nextY : [Int] = []
+        nextX.append(row)
+        nextY.append(column)
+        
+        while nextX.count != 0 {
+            
+            let x = nextX[0], y = nextY[0]
+            nextX.removeFirst()
+            nextY.removeFirst()
+            
+            result[x][y] = newColor
+            visited[x][y] = true
+            
+            if (x - 1 >= 0 && image[x - 1][y] == image[x][y] && visited[x - 1][y] == false){
+                nextX.append(x - 1)
+                nextY.append(y)
+            }
+            if (x + 1 < rows && image[x + 1][y] == image[x][y] && visited[x + 1][y] == false){
+                nextX.append(x + 1)
+                nextY.append(y)
+            }
+            if (y - 1 >= 0 && image[x][y - 1] == image[x][y] && visited[x][y - 1] == false){
+                nextX.append(x)
+                nextY.append(y - 1)
+            }
+            if (y + 1 < columns && image[x][y + 1] == image[x][y] && visited[x][y + 1] == false){
+                nextX.append(x)
+                nextY.append(y + 1)
+            }
+        
+        }
+        
+        return result
+        
     }
 }
